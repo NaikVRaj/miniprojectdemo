c207087964774999b82f1b698f5a365c9b88ab5e
NaikVRaj <156048534+NaikVRaj@users.noreply.github.com>
2024-02-13 15:02:48 +0530

    Update calculator.java

--- src/main/java/com/example/spe_task/calculator.java
+++ src/main/java/com/example/spe_task/calculator.java
@@ -135,7 +135,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Calculator {
+public class calculator {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(Calculator.class, args);
